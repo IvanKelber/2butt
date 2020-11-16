6d128000de3044e488a2f9611e6a7e274e8f9155
Ivan Kelber <IvanKelber@gmail.com>
2020-11-16 14:20:42 -0500

    working controller and flippy square.

---
+++ Assets/Scripts/FlipSquare.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSquare : MonoBehaviour
+{
+    public float torqueSpeed = 1000;
+    public float angularVelocityCap = 100;
+    public float flipDuration;
+    Rigidbody2D body;
+
+    bool flipping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L)) {
+            StartCoroutine(Flip(-1, flipDuration));
+        }
+        if(Input.GetKeyDown(KeyCode.J)) {
+            StartCoroutine(Flip(1, flipDuration));
+        }
+    }
+
+    void FixedUpdate() {
+
+    }
+
+    public void GetKicked(int direction) {
+        StartCoroutine(Flip(-direction, flipDuration));
+    }
+
+    IEnumerator Flip(int direction, float duration) {
+        if(!flipping) {
+            flipping = true;
+            float start = Time.time;
+            float end = start + duration;
+            Debug.Log(start +", " + end);
+            while(Time.time < end) {
+                if(Mathf.Abs(body.angularVelocity) <= angularVelocityCap) {
+                    float torque = direction * torqueSpeed * Time.fixedDeltaTime;
+                    body.AddTorque(torque);
+                }
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        flipping = false;
+        yield return null;
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    [Range(.05f, 10)]
+    public float minJumpHeight = 3;
+    [Range(.05f,10)]
+    public float timeToJumpApex = 1;
+    [Range(0,100)]
+    public float maxAcceleration = 25, maxAirAcceleration = 75;
+
+    [Range(.01f, 2)]
+    public float kickDistance = .5f;
+
+    public LayerMask squareMask;
+
+
+    float maxJumpVelocity, minJumpVelocity;
+
+    Vector2 playerInput;
+    Vector2 desiredVelocity;
+    bool desiredJump;
+    bool desiredFloat;
+    bool onGround;
+    bool desiredKick;
+
+    int playerDirection = 1;
+
+    Vector2 velocity;
+    Rigidbody2D body;
+    BoxCollider2D collider;
+    Bounds bounds;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        maxJumpVelocity = Mathf.Abs(Physics2D.gravity.y * timeToJumpApex);
+        minJumpVelocity = Mathf.Sqrt(-2 * Physics2D.gravity.y * minJumpHeight);
+    }
+
+    void Update () {
+        UpdateBounds();
+		playerInput.x = Input.GetAxis("Horizontal");
+		// playerInput.y = Input.GetAxis("Vertical");
+		playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+		desiredVelocity =
+			new Vector2(playerInput.x, 0) * maxSpeed;
+
+        desiredJump |= Input.GetKeyDown(KeyCode.W);
+        if(Input.GetKeyUp(KeyCode.W)) {
+            desiredFloat = true;
+        }
+
+        desiredKick |= Input.GetKeyDown(KeyCode.Space);
+        if(desiredKick) {
+            Debug.Log("Desired Kick");
+            Kick();
+            desiredKick = false;
+        }
+	}
+
+	void FixedUpdate () {
+		velocity = body.velocity;
+		float maxSpeedChange = maxAcceleration * Time.deltaTime;
+		velocity.x =
+			Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        if(velocity.x != 0) {
+            playerDirection = (int) Mathf.Sign(velocity.x);
+        }
+
+        if(desiredJump) {
+            desiredJump = false;
+            Jump();
+        }
+        if(desiredFloat) {
+            if(velocity.y > minJumpVelocity) {
+                velocity.y = minJumpVelocity;
+            }
+        }
+		body.velocity = velocity;
+        onGround = false;
+        desiredFloat = false;
+	}
+
+    void Jump() {
+        if(onGround) {
+            velocity.y += maxJumpVelocity;
+        } else { 
+            Debug.Log("Not on ground");
+        }
+    }
+
+    void Kick() {
+        FlipSquare square = CheckForSquare();
+        if(square) {
+            square.GetKicked(playerDirection);
+        }
+    }
+
+    void UpdateBounds() {
+        bounds = collider.bounds;
+    }
+
+
+
+    FlipSquare CheckForSquare() {
+        Vector2 leftBottom = new Vector2(bounds.center.x, bounds.center.y) + 
+            (Vector2.down * bounds.extents) + (Vector2.left * bounds.extents);
+        Vector2 rightBottom = new Vector2(bounds.center.x, bounds.center.y) + 
+            (Vector2.down * bounds.extents) + (Vector2.right * bounds.extents);        
+
+        for(int i = 0; i < 10; i++) {
+            Vector2 rayOrigin = (playerDirection == 1 ? rightBottom : leftBottom) + Vector2.up * i * (bounds.size.x/10);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * playerDirection * kickDistance, Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * playerDirection, kickDistance, squareMask);
+
+            if(hit) {
+                if(hit.collider != null) {
+                    Debug.Log("Found square");
+                    return hit.collider.gameObject.GetComponent<FlipSquare>() as FlipSquare;
+                }
+            }
+        }
+        return null;
+    }
+
+    void OnCollisionEnter2D () {
+		onGround = true;
+	}
+
+    void OnCollisionStay2D() {
+        onGround = true;
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Vector2 leftBottom = new Vector2(bounds.center.x, bounds.center.y) + 
+            (Vector2.down * bounds.extents) + (Vector2.left * bounds.extents);
+        Vector2 rightBottom = new Vector2(bounds.center.x, bounds.center.y) + 
+            (Vector2.down * bounds.extents) + (Vector2.right * bounds.extents);        
+
+        for(int i = 0; i < 10; i++) {
+            Vector2 rayOrigin = (playerDirection == 1 ? rightBottom : leftBottom) + Vector2.up * i * (bounds.size.x/10);
+
+            Gizmos.DrawRay(rayOrigin, Vector2.right * playerDirection * kickDistance);
+        }
+    }
+
+}
