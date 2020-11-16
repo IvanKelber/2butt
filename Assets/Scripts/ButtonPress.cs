e1c6a6ae3329fcd6e93b642b09f5e66889f53207
Ivan Kelber <IvanKelber@gmail.com>
2020-11-16 17:36:15 -0500

    added buttons and triggers

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+public class ButtonPress : MonoBehaviour
+{
+    BoxCollider2D collider;
+
+    [SerializeField]
+    GameEvent onPress, onUnpress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+
+    void OnTriggerEnter2D() {
+        onPress.Raise();
+    }
+
+    void OnTriggerExit2D() {
+        onUnpress.Raise();
+    }
+}
---
+++ Assets/Scripts/ShrinkDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkDoor : MonoBehaviour
+{
+
+    [Range(0, 5)]
+    public float shrinkDuration = 1;
+
+    public void Shrink() {
+        StartCoroutine(DoShrink());
+    }
+
+    IEnumerator DoShrink() {
+        float start = Time.time;
+        float end = start + shrinkDuration;
+        Vector3 originalScale = transform.localScale;
+        Vector3 goalScale = new Vector3(originalScale.x, 0, originalScale.z);
+        while(Time.time < end) {
+            // Shrink only on y axis;
+            float percentComplete = (end - Time.time)/(shrinkDuration);
+            percentComplete = Mathf.Clamp01(percentComplete);
+            transform.localScale = Vector3.Lerp(goalScale, originalScale, percentComplete);
+            yield return null;
+        }
+        Destroy(this.gameObject);
+    }
+}
