62046ffee85681f0b19aedbc7713ebfa2a74db35
Ivan Kelber <IvanKelber@gmail.com>
2020-11-18 15:15:41 -0500

    added some transitions

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,21 +8,44 @@ public class LevelLoader : MonoBehaviour
 
     public List<GameObject> levelPrefabs;
 
+    private Fade fade;
     int levelId = 0;
     GameObject currentLevel;
     // Start is called before the first frame update
     void Start()
     {
         currentLevel = Instantiate(levelPrefabs[0], Vector3.zero, Quaternion.identity);
+        fade = GetComponent<Fade>() as Fade;
     }
 
     public void LoadNextLevel() {
         levelId++;
         if(levelId < levelPrefabs.Count) {
-            Destroy(currentLevel);
-            currentLevel = Instantiate(levelPrefabs[levelId], Vector3.zero, Quaternion.identity);
+            StartCoroutine(LoadLevel(levelId));
         } else {
-            SceneManager.LoadScene(0);
+            StartCoroutine(LoadStartMenu());
+        }
+    }
+
+    IEnumerator LoadStartMenu() {
+        if(fade != null) {
+            Debug.Log("fading out");
+            yield return StartCoroutine(fade.FadeOut());
+        }
+        SceneManager.LoadScene(0);
+        
+    }
+
+    IEnumerator LoadLevel(int levelId) {
+        if(fade != null) {
+            Debug.Log("fading out");
+            yield return StartCoroutine(fade.FadeOut());
+        }
+        Destroy(currentLevel);
+        currentLevel = Instantiate(levelPrefabs[levelId], Vector3.zero, Quaternion.identity);
+        if(fade != null) {
+            Debug.Log("Fading in");
+            yield return StartCoroutine(fade.FadeInInverted());
         }
     }
 
---
+++ Assets/Transitions/Scripts/Fade.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    [Range(0.1f, 5)]
+    public float fadeDuration = 1;
+    public ScreenTransitionImageEffect imageEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(imageEffect == null)
+            imageEffect = GetComponent<ScreenTransitionImageEffect>() as ScreenTransitionImageEffect;
+    }
+
+    void Update() {
+
+    }
+
+    public IEnumerator FadeOut() {
+        float start = Time.time;
+        float end = start + fadeDuration;
+        imageEffect.maskInvert = false;
+        imageEffect.maskValue = 0;
+        imageEffect.Enable();
+        // Fade Out
+        while(Time.time < end) {
+            float percentage = 1 - (end - Time.time)/fadeDuration;
+            imageEffect.maskValue = Mathf.Lerp(0,1, percentage);
+            yield return null;
+        }
+        imageEffect.maskValue = 1;
+        yield return null;
+    }
+
+    public IEnumerator FadeInInverted() {
+        imageEffect.maskInvert = true;
+        imageEffect.maskValue = 0;
+        float start = Time.time;
+        float end = start + fadeDuration;
+        //Fade in
+        while(Time.time < end) {
+            float percentage = 1 - (end - Time.time)/fadeDuration;
+            imageEffect.maskValue = Mathf.Lerp(0,1, percentage);
+            yield return null;
+        }
+        imageEffect.maskValue = 1;
+        imageEffect.Disable();
+        yield return null;
+    }
+}
