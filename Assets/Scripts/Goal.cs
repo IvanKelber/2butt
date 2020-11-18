8337215045245720ab2e4460e4043808f43b51a2
Ivan Kelber <IvanKelber@gmail.com>
2020-11-17 19:44:26 -0500

    Added level loader with 3 levels

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+
+public class Goal : MonoBehaviour
+{
+
+    public GameEvent onGoalReached;
+    public LayerMask playerMask;
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if((playerMask.value & 1 << collision.gameObject.layer) != 0) {
+            onGoalReached.Raise();
+        }
+    }
+
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public List<GameObject> levelPrefabs;
+
+    int levelId = 0;
+    GameObject currentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = Instantiate(levelPrefabs[0], Vector3.zero, Quaternion.identity);
+    }
+
+    public void LoadNextLevel() {
+        levelId++;
+        if(levelId < levelPrefabs.Count) {
+            Destroy(currentLevel);
+            currentLevel = Instantiate(levelPrefabs[levelId], Vector3.zero, Quaternion.identity);
+        }
+    }
+
+}
